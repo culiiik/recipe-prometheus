8f20e6bedc266685e695911ff5338175a5c248d6
culiiik <98949622+culiiik@users.noreply.github.com>
2023-11-16 14:53:25 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,5 +3,6 @@
 # TODO: remove once custom health checks are available
 while true
 do
-  printf 'HTTP/1.1 200 OK\n\n%s' "ok" | nc -6 -v -l -N 1999
+  sleep 600
+  # printf 'HTTP/1.1 200 OK\n\n%s' "ok" | nc -6 -v -l -N 1999
 done
